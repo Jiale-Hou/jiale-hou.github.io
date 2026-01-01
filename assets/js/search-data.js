af3e082fd7762d1468fb653fc93628f43ab5ed65
Jiale-Hou <160786050+Jiale-Hou@users.noreply.github.com>
2026-01-01 07:15:52 +0000

    Deploying to gh-pages from @ Jiale-Hou/jiale-hou.github.io@aa063174fd2d17cfbcc1b247e931b41d9e664cc7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -461,10 +461,7 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
-            },},{id: "talks-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "Talks",},{
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
